58cbcd0105d741c0c1aaa7ef5dcf3c31d07d91f3
18650 <tim.bauer.1998@gmail.com>
2023-04-25 15:31:06 +0200

    add text with x,y,r

--- test.py
+++ test.py
@@ -7,11 +7,13 @@ def resize(img):
         return cv2.resize(img, (512, 512))
 
 
-cap = cv2.VideoCapture(0)
-
+cap = cv2.VideoCapture(1)
 
 while True:
+    
+
     rat,frame = cap.read()
+    cv2.imshow("Camera", resize(frame))
     #img = cv2.VideoCapture(0)
    # l_b=np.array([0,230,170])# lower hsv bound for red
    # u_b=np.array([255,255,220])# upper hsv bound to red
@@ -29,25 +31,27 @@ while True:
     # Apply Hough transform on the blurred image.
     detected_circles = cv2.HoughCircles(gray, 
                        cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-                   param2 = 30, minRadius = 45, maxRadius = 55)
+                   param2 = 30, minRadius = 19, maxRadius = 23)
     
     # Draw circles that are detected.
     if detected_circles is not None:
-    
+            
         # Convert the circle parameters a, b and r to integers.
         detected_circles = np.uint16(np.around(detected_circles))
     
         for pt in detected_circles[0, :]:
             a, b, r = pt[0], pt[1], pt[2]
-    
+            #outpt = ''
+            #outpt = pt
             # Draw the circumference of the circle.
             cv2.circle(frame, (a, b), r, (0, 255, 0), 2)
-    
+            cv2.putText(frame,str(pt),[a,b],cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255))
             # Draw a small circle (of radius 1) to show the center.
-           # cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
+            cv2.circle(frame, (a, b), 1, (0, 0, 255), 3)
             print(a,b)
-            cv2.imshow("Detected Circle", resize(frame))
-            
+            #cv2.imshow("Detected Circle", resize(frame))
+
+    cv2.imshow("Detected Circle", resize(frame))         
 
     key=cv2.waitKey(1)
     if key==ord('q'):
